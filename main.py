651069d2c30bcf991d37deddb1d96b2e59d0cf80
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2024-01-24 16:46:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ def ui():
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">', unsafe_allow_html=True)
 
     st.title('Consulta Github')
-    usuario = st.text_input('Insira o username de usuario github')
+    username = st.text_input('Insira o username de usuario github')
 
     if(st.button('buscar')):
-        infoUsuario = selecionarUsuario(usuario)
+        infoUsuario = selecionarUsuario(username)
         if(infoUsuario is not None):
             st.markdown(f'''                        
                 <div class="card" style="width: 18rem; justify-content: center;">
